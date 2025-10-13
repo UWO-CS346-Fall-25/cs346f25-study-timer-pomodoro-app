91c3590e6296d2bc7b76467bba5fe54ebf2d2567
ab <calebemmanuel75@gmail.com>
2025-10-13 12:10:27 -0500

    Make local Postgres defaults(environment variables) and rerunnable migration.

--- db/migrations/001_create_users_table.sql
+++ db/migrations/001_create_users_table.sql
@@ -27,5 +27,8 @@ BEGIN
 END;
 $$ language 'plpgsql';
 
+-- Ensure the trigger can be recreated safely if it already exists
+DROP TRIGGER IF EXISTS update_users_updated_at ON users;
+
 CREATE TRIGGER update_users_updated_at BEFORE UPDATE ON users
 FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
